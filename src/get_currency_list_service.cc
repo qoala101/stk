2d47b2cd0838b8ac7d1e8c2dd64a5cea3c98f882
Volodymyr Hromakov <4y5t6r@gmail.com>
2022-03-17 15:24:38 +0200

    Get currencies now provides only currency names.

--- src/get_currency_list_service.cc
+++ src/get_currency_list_service.cc
@@ -5,15 +5,56 @@
 
 namespace
 {
+  std::optional<web::json::value> ExchangeInfoToCurrencyList(const web::json::value &exchange_info)
+  {
+    if (!exchange_info.has_field("symbols"))
+    {
+      return std::nullopt;
+    }
+
+    const auto &currency_info_value = exchange_info.at("symbols");
+
+    if (!currency_info_value.is_array())
+    {
+      return std::nullopt;
+    }
+
+    const auto &currency_info_array = currency_info_value.as_array();
+    const auto has_symbol_field = [](const web::json::value &value)
+    {
+      return value.has_string_field("symbol");
+    };
+    const auto get_symbol_field = [](const web::json::value &value)
+    {
+      return value.at("symbol").as_string();
+    };
+
+    auto currencies_range = currency_info_array | (std::views::filter(has_symbol_field) | std::views::transform(get_symbol_field));
+    const auto currency_names = std::vector<web::json::value>(currencies_range.begin(), currencies_range.end());
+    return web::json::value::array(currency_names);
+  }
+
   void HandleGetRequest(const web::http::http_request &request)
   {
     spdlog::info(BOOST_CURRENT_FUNCTION);
 
-    spdlog::info("Getting currency list...");
+    spdlog::info("Requesting exchange info...");
     auto http_client = web::http::client::http_client{"https://api.binance.com/api/v3/exchangeInfo"};
-    auto request_task = http_client.request(web::http::methods::GET);
-    auto response = request_task.get();
-    request.reply(web::http::status_codes::OK, response.extract_json().get());
+    const auto response = http_client.request(web::http::methods::GET).get();
+    const auto exchange_info = response.extract_json().get();
+    spdlog::info("Got exchange info response");
+
+    const auto currency_list = ExchangeInfoToCurrencyList(exchange_info);
+
+    if (!currency_list.has_value())
+    {
+      spdlog::info("Exchange info is broken");
+      request.reply(web::http::status_codes::NotFound);
+      return;
+    }
+
+    request.reply(web::http::status_codes::OK, currency_list.value());
+    spdlog::info("Extracted currency list");
   }
 }
 
