e3d3c9926337b7b64c251e3881c5d1a80c892cb9
Volodymyr Hromakov <4y5t6r@gmail.com>
2022-03-30 13:57:40 +0300

    Added support for numeric, enum, and optional parameters to rest request API.

--- include/rest_request.h
+++ include/rest_request.h
@@ -5,28 +5,58 @@
 #include <cpprest/json.h>
 #include <cpprest/uri_builder.h>
 
+#include <boost/lexical_cast.hpp>
+#include <magic_enum.hpp>
 #include <optional>
 #include <string_view>
 
+template <typename T>
+concept Number = std::is_integral_v<T> || std::is_floating_point_v<T>;
+
+template <typename T>
+concept Enumeration = std::is_enum_v<T>;
+
 namespace stonks::rest {
-class RestRequest {
- public:
-  explicit RestRequest(std::string_view uri);
-  explicit RestRequest(web::http::method method, std::string_view uri);
+  class RestRequest {
+   public:
+    explicit RestRequest(std::string_view uri);
+    explicit RestRequest(web::http::method method, std::string_view uri);
 
-  RestRequest &AppendUri(std::string_view uri);
+    RestRequest &AppendUri(std::string_view uri);
 
-  RestRequest &AddParameter(std::string_view key, std::string_view value);
-  RestRequest &AddHeader(std::string_view key, std::string_view value);
+    RestRequest &AddParameter(std::string_view key, std::string_view value);
 
-  const std::string &GetParametersAsString() const;
+    template <Number T>
+    RestRequest &AddParameter(std::string_view key, T value) {
+      return AddParameter(key, boost::lexical_cast<std::string>(value));
+    }
 
-  std::optional<web::json::value> SendAndGetResponse() const;
+    template <Enumeration T>
+    RestRequest &AddParameter(std::string_view key, T value) {
+      return AddParameter(key, magic_enum::enum_name<T>(value));
+    }
 
- private:
-  web::http::http_request http_request_{};
-  web::uri_builder uri_builder_{};
-};
+    template <typename T>
+    RestRequest &AddParameter(std::string_view key,
+                              const std::optional<T> &optional_value) {
+      if (optional_value.has_value()) {
+        return AddParameter(key, *optional_value);
+      }
+
+      return *this;
+    }
+
+    RestRequest &AddHeader(std::string_view key, std::string_view value);
+
+    std::string GetUri() const;
+    const std::string &GetParametersAsString() const;
+
+    std::optional<web::json::value> SendAndGetResponse() const;
+
+   private:
+    web::http::http_request http_request_{};
+    web::uri_builder uri_builder_{};
+  };
 }  // namespace stonks::rest
 
 #endif  // STONKS_REST_REQUEST_H_
---
+++ tests/rest_request.cc
@@ -0,0 +1,78 @@
+#define _TURN_OFF_PLATFORM_STRING
+
+#include "rest_request.h"
+
+#include <gtest/gtest.h>
+
+#include <magic_enum.hpp>
+
+TEST(RestRequest, AppendUri) {
+  const auto result_uri =
+      stonks::rest::RestRequest{"base_uri"}.AppendUri("appended_uri").GetUri();
+  const auto exprected_uri = "base_uri/appended_uri";
+  EXPECT_EQ(result_uri, exprected_uri);
+}
+
+namespace {
+enum class DefaultNameEnum { kDefaultEnumName };
+enum class CustomNameEnum { kCustomEnumName };
+}  // namespace
+
+template <>
+constexpr std::string_view magic_enum::customize::enum_name<CustomNameEnum>(
+    CustomNameEnum value) noexcept {
+  switch (value) {
+    case CustomNameEnum::kCustomEnumName:
+      return "CUSTOM_ENUM_NAME";
+    default:
+      break;
+  }
+
+  return {};
+}
+
+TEST(RestRequest, ParameterTypesToString) {
+  const auto result_string =
+      stonks::rest::RestRequest{""}
+          .AddParameter("string", "abc")
+          .AddParameter("int", 123456789)
+          .AddParameter("int64_t", int64_t{123456789})
+          .AddParameter("float", float{123456789.123456789})
+          .AddParameter("double", double{123456789.123456789})
+          .AddParameter("default_enum_name", DefaultNameEnum::kDefaultEnumName)
+          .AddParameter("custom_enum_name", CustomNameEnum::kCustomEnumName)
+          .AddParameter("optional_string", std::optional<std::string>{"abc"})
+          .AddParameter("optional_int", std::optional<int>{123456789})
+          .AddParameter("optional_int64_t",
+                        std::optional<int64_t>{int64_t{123456789}})
+          .AddParameter("optional_float",
+                        std::optional<float>{float{123456789.123456789}})
+          .AddParameter("optional_double",
+                        std::optional<double>{double{123456789.123456789}})
+          .AddParameter(
+              "optional_default_enum_name",
+              std::optional<DefaultNameEnum>{DefaultNameEnum::kDefaultEnumName})
+          .AddParameter(
+              "optional_custom_enum_name",
+              std::optional<CustomNameEnum>{CustomNameEnum::kCustomEnumName})
+          .AddParameter("no_option", std::optional<int>{std::nullopt})
+          .GetParametersAsString();
+  const auto expected_string =
+      "string=abc&"
+      "int=123456789&"
+      "int64_t=123456789&"
+      "float=123456792&"
+      "double=123456789.12345679&"
+      "default_enum_name=kDefaultEnumName&"
+      "custom_enum_name=CUSTOM_ENUM_NAME&"
+      "optional_string=abc&"
+      "optional_int=123456789&"
+      "optional_int64_t=123456789&"
+      "optional_float=123456792&"
+      "optional_double=123456789.12345679&"
+      "optional_default_enum_name=kDefaultEnumName&"
+      "optional_custom_enum_name=CUSTOM_ENUM_NAME";
+  EXPECT_EQ(result_string, expected_string);
+}
+
+#undef _TURN_OFF_PLATFORM_STRING
