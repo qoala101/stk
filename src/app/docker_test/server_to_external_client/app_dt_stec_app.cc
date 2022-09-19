5c807bb3175dd23f88200f3a2853d2bc5695da61
Volodymyr Hromakov <4y5t6r@gmail.com>
2022-09-19 13:11:53 +0300

    Added connection between pds and stec apps.

--- src/app/docker_test/server_to_external_client/app_dt_stec_app.cc
+++ src/app/docker_test/server_to_external_client/app_dt_stec_app.cc
@@ -3,12 +3,20 @@
 #include <utility>
 
 #include "app_dt_stec_binance_types.h"
+#include "cpp_chrono.h"
 
 namespace stonks::app::dt::stec {
-App::App(binance::Client binance_client)
-    : binance_client_{std::move(binance_client)} {}
+App::App(binance::Client binance_client, PdsAppClient pds_app_client)
+    : binance_client_{std::move(binance_client)},
+      pds_app_client_{std::move(pds_app_client)} {}
 
-auto App::GetAveragePrice(const Symbol &symbol) const -> Price {
-  return {binance_client_.GetCurrentAveragePrice(symbol).price};
+auto App::GetAveragePrice(Symbol symbol) const -> Price {
+  auto price = Price{binance_client_.GetCurrentAveragePrice(symbol).price};
+
+  pds_app_client_.RecordSymbolPrice({.time = cpp::GetCurrentTime(),
+                                     .symbol = std::move(symbol),
+                                     .price = price});
+
+  return price;
 }
 }  // namespace stonks::app::dt::stec
--- src/app/docker_test/server_to_external_client/app_dt_stec_main.cc
+++ src/app/docker_test/server_to_external_client/app_dt_stec_main.cc
@@ -1,24 +1,31 @@
+#include <boost/di.hpp>
 #include <cstdio>
 #include <memory>
 #include <tuple>
 
-#include "app_dt_stec_app.h"
 #include "app_dt_stec_app_server.h"
+#include "app_dt_stec_pds_app_client.h"
 #include "app_log_spdlog_injector.h"
 #include "app_network_restsdk_injector.h"
 #include "cpp_di_bind_type_to_value.h"
+#include "cpp_di_make_injector.h"
+#include "cpp_di_override_bindings_for_type.h"
 #include "cpp_not_null.h"
-#include "cpp_smart_pointers.h"
 #include "log_i_logger.h"
 #include "network_types.h"
 #include "not_null.hpp"
 
 auto main(int /*unused*/, char* /*unused*/[]) -> int {
-  const auto injector = stonks::cpp::di::MakeInjector(
+  auto default_injector = stonks::cpp::di::MakeInjector(
       stonks::app::injectors::MakeNetworkRestsdkInjector(),
       stonks::app::injectors::MakeLogSpdlogInjector(),
       stonks::cpp::di::BindTypeToValue<stonks::network::Uri>(
-          stonks::network::Uri{"http://localhost:6506"}));
+          stonks::network::Uri{"http://localhost:6507"}));
+
+  const auto injector = stonks::cpp::di::OverrideBindingsForType<
+      stonks::app::dt::stec::PdsAppClient>(
+      default_injector, stonks::cpp::di::BindTypeToValue<stonks::network::Uri>(
+                            stonks::network::Uri{"http://localhost:6506"}));
 
   const auto logger =
       injector.create<stonks::cpp::NnUp<stonks::log::ILogger>>();
