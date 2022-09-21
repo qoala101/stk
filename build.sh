210f39ca377796a75f2d6e753ea499aff92f73a8
Volodymyr Hromakov <4y5t6r@gmail.com>
2022-09-21 19:55:34 +0300

    Added intermediate docker file for libs and base image for apps. Reduced size of dependencies image. Added CLI11 lib for args parsing. Added args to docker test apps. Moved fetch not null to the main cmake file. Added options to cmake file. Moved docker-compose for docker test app to the docker app folder.

--- build.sh
+++ build.sh
@@ -1,16 +1,29 @@
-# if conanfile.txt changed, build stonks-deps image
+# Build an image which should be used as base for all apps.
+# Contains base OS image and packages required for networking to work.
+# No need for rebuild.
+docker build -t qoala101/stonks:base -f ./docker/base/Dockerfile .
 
-# build app images
-docker compose build
+# Build dependencies image.
+# Should be rebuilt when new packages are added to conanfile.txt or to FetchContent section of CMakeFile.txt.
+docker build -t qoala101/stonks:dependencies -f ./docker/dependencies/Dockerfile .
 
-# upload app images to docker hub
-docker tag c2ec5e59b2f4 qoala101/stonks:stonks-stec
-docker tag 2d60754852e4 qoala101/stonks:stonks-pds
-docker push qoala101/stonks:stonks-stec
-docker push qoala101/stonks:stonks-pds
+# Build image which contains all project binaries.
+# Should be rebuilt when dependencies image or any source files are changed.
+docker build -t qoala101/stonks:binaries -f ./docker/binaries/Dockerfile .
+
+# Build Docker test app images.
+# Should be rebuild when binaries image is changed.
+docker compose -f ./src/app/docker_test/docker-compose.yml build
+
+# Upload Docker test app images to Docker Hub.
+docker compose -f ./src/app/docker_test/docker-compose.yml push
+
+
+
 
 # run kubernetes deployment
 kompose convert
+# use something like all yaml files 
 kubectl apply -f pds-tcp-service.yaml,pds-pod.yaml,stec-tcp-service.yaml,stec-pod.yaml
 
 # run ngrok which opens port to kubernetes
