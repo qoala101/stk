985848272e3eac7ca2af9ca84a4e4c32dd973e25
Volodymyr Hromakov <4y5t6r@gmail.com>
2022-09-22 09:53:14 +0300

    Added command line args parser via CLI11.

--- src/app/docker_test/persistent_db_server/app_dt_pds_main.cc
+++ src/app/docker_test/persistent_db_server/app_dt_pds_main.cc
@@ -1,7 +1,9 @@
+#include <CLI/App.hpp>
+#include <CLI/Config.hpp>     // IWYU pragma: keep
+#include <CLI/Formatter.hpp>  // IWYU pragma: keep
 #include <boost/di.hpp>
-#include <cstdio>
 #include <memory>
-#include <tuple>
+#include <utility>
 
 #include "app_dt_pds_app_server.h"
 #include "app_log_spdlog_injector.h"
@@ -10,28 +12,38 @@
 #include "app_wait_for_interrupt.h"
 #include "cpp_di_bind_type_to_value.h"
 #include "cpp_di_make_injector.h"
+#include "cpp_format.h"
 #include "cpp_not_null.h"
 #include "log_i_logger.h"
 #include "network_types.h"
 #include "not_null.hpp"
 #include "sqlite_types.h"
 
-auto main(int /*unused*/, char* /*unused*/[]) -> int {
+auto main(int argc, char* argv[]) -> int {
+  auto app = CLI::App{};
+
+  auto port = 6506;
+  app.add_option("--port", port);
+
+  app.parse(argc, argv);
+
+  const auto app_name = app.get_name();
+
   const auto injector = stonks::cpp::di::MakeInjector(
       stonks::app::injectors::MakeNetworkRestsdkInjector(),
       stonks::app::injectors::MakeSqldbSqliteInjector(),
       stonks::app::injectors::MakeLogSpdlogInjector(),
       stonks::cpp::di::BindTypeToValue<stonks::network::Uri>(
-          stonks::network::Uri{"http://0.0.0.0:6506"}),
+          stonks::network::Uri{stonks::cpp::Format("http://0.0.0.0:{}", port)}),
       stonks::cpp::di::BindTypeToValue<stonks::sqlite::FilePath>(
           stonks::sqlite::FilePath{"persistent_db_server.db"}));
 
   const auto logger =
       injector.create<stonks::cpp::NnUp<stonks::log::ILogger>>();
-  logger->LogImportantEvent("Started: persistent_db_server");
+  logger->LogImportantEvent(stonks::cpp::Format("Started: {}", app_name));
 
   const auto server = injector.create<stonks::app::dt::pds::AppServer>();
 
   stonks::app::WaitForInterrupt();
-  logger->LogImportantEvent("Ended: persistent_db_server");
+  logger->LogImportantEvent(stonks::cpp::Format("Ended: {}", app_name));
 }
--- src/app/docker_test/server_to_external_client/app_dt_stec_main.cc
+++ src/app/docker_test/server_to_external_client/app_dt_stec_main.cc
@@ -1,5 +1,10 @@
+#include <CLI/App.hpp>
+#include <CLI/Config.hpp>     // IWYU pragma: keep
+#include <CLI/Formatter.hpp>  // IWYU pragma: keep
 #include <boost/di.hpp>
 #include <memory>
+#include <string>
+#include <utility>
 
 #include "app_dt_stec_app_server.h"
 #include "app_dt_stec_pds_app_client.h"
@@ -9,29 +14,47 @@
 #include "cpp_di_bind_type_to_value.h"
 #include "cpp_di_make_injector.h"
 #include "cpp_di_override_bindings_for_type.h"
+#include "cpp_format.h"
 #include "cpp_not_null.h"
 #include "log_i_logger.h"
 #include "network_types.h"
 #include "not_null.hpp"
 
-auto main(int /*unused*/, char* /*unused*/[]) -> int {
+auto main(int argc, char* argv[]) -> int {
+  auto app = CLI::App{};
+
+  auto port = 6507;
+  app.add_option("--port", port);
+
+  auto pds_host = std::string{"0.0.0.0"};
+  app.add_option("--pds_host", pds_host);
+
+  auto pds_port = 6506;
+  app.add_option("--pds_port", pds_port);
+
+  app.parse(argc, argv);
+
+  const auto app_name = app.get_name();
+
   auto default_injector = stonks::cpp::di::MakeInjector(
       stonks::app::injectors::MakeNetworkRestsdkInjector(),
       stonks::app::injectors::MakeLogSpdlogInjector(),
       stonks::cpp::di::BindTypeToValue<stonks::network::Uri>(
-          stonks::network::Uri{"http://0.0.0.0:6507"}));
+          stonks::network::Uri{
+              stonks::cpp::Format("http://0.0.0.0:{}", port)}));
 
   const auto injector = stonks::cpp::di::OverrideBindingsForType<
       stonks::app::dt::stec::PdsAppClient>(
       default_injector, stonks::cpp::di::BindTypeToValue<stonks::network::Uri>(
-                            stonks::network::Uri{"http://pds:6506"}));
+                            stonks::network::Uri{stonks::cpp::Format(
+                                "http://{}:{}", pds_host, pds_port)}));
 
   const auto logger =
       injector.create<stonks::cpp::NnUp<stonks::log::ILogger>>();
-  logger->LogImportantEvent("Started: server_to_external_client");
+  logger->LogImportantEvent(stonks::cpp::Format("Started: {}", app_name));
 
   const auto server = injector.create<stonks::app::dt::stec::AppServer>();
 
   stonks::app::WaitForInterrupt();
-  logger->LogImportantEvent("Ended: server_to_external_client");
+  logger->LogImportantEvent(stonks::cpp::Format("Ended: {}", app_name));
 }
