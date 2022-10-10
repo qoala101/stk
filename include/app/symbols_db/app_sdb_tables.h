9d749571673e7ec07607c2d6b244cc13efc3dbee
Volodymyr Hromakov <4y5t6r@gmail.com>
2022-10-10 22:56:28 +0300

    Added string constants to tables.

--- include/app/symbols_db/app_sdb_tables.h
+++ include/app/symbols_db/app_sdb_tables.h
@@ -7,17 +7,42 @@ namespace stonks::app::sdb::tables {
 /**
  * @copydoc core::Asset
  */
-[[nodiscard]] auto Asset() -> const sqldb::TableDefinition &;
+struct Asset {
+  [[nodiscard]] static auto Definition() -> const sqldb::TableDefinition &;
+
+  static constexpr auto kTable = "Asset";
+  static constexpr auto kId = "id";
+  static constexpr auto kName = "name";
+};
 
 /**
  * @copydoc core::SymbolInfo
  */
-[[nodiscard]] auto SymbolInfo() -> const sqldb::TableDefinition &;
+struct SymbolInfo {
+  [[nodiscard]] static auto Definition() -> const sqldb::TableDefinition &;
+
+  static constexpr auto kTable = "SymbolInfo";
+  static constexpr auto kId = "id";
+  static constexpr auto kName = "name";
+  static constexpr auto kBaseAssetId = "base_asset_id";
+  static constexpr auto kBaseAssetMinAmount = "base_asset_min_amount";
+  static constexpr auto kBaseAssetPriceStep = "base_asset_price_step";
+  static constexpr auto kQuoteAssetId = "quote_asset_id";
+  static constexpr auto kQuoteAssetMinAmount = "quote_asset_min_amount";
+  static constexpr auto kQuoteAssetPriceStep = "quote_asset_price_step";
+};
 
 /**
  * @copydoc core::SymbolPriceRecord
  */
-[[nodiscard]] auto SymbolPriceRecord() -> const sqldb::TableDefinition &;
+struct SymbolPriceRecord {
+  [[nodiscard]] static auto Definition() -> const sqldb::TableDefinition &;
+
+  static constexpr auto kTable = "SymbolPriceRecord";
+  static constexpr auto kSymbolId = "symbol_id";
+  static constexpr auto kPrice = "price";
+  static constexpr auto kTime = "time";
+};
 }  // namespace stonks::app::sdb::tables
 
 #endif  // STONKS_APP_SYMBOLS_DB_APP_SDB_TABLES_H_
--- src/app/symbols_db/app_sdb_tables.cc
+++ src/app/symbols_db/app_sdb_tables.cc
@@ -6,56 +6,58 @@
 #include "sqldb_enums.h"
 
 namespace stonks::app::sdb::tables {
-auto Asset() -> const sqldb::TableDefinition& {
-  static const auto kTable =
-      sqldb::TableDefinition{.table = {"Asset"},
-                             .columns = {{.column = {"id"},
+auto Asset::Definition() -> const sqldb::TableDefinition& {
+  static const auto kDefinition =
+      sqldb::TableDefinition{.table = {kTable},
+                             .columns = {{.column = {kId},
                                           .data_type = sqldb::DataType::kInt64,
                                           .primary_key = true,
                                           .auto_increment = true,
                                           .unique = true},
-                                         {.column = {"name"},
+                                         {.column = {kName},
                                           .data_type = sqldb::DataType::kString,
                                           .unique = true}}};
-  return kTable;
+  return kDefinition;
 }
 
-auto SymbolInfo() -> const sqldb::TableDefinition& {
-  static const auto kTable = sqldb::TableDefinition{
-      .table = {"SymbolInfo"},
-      .columns = {{.column = {"id"},
-                   .data_type = sqldb::DataType::kInt64,
-                   .primary_key = true,
-                   .auto_increment = true,
-                   .unique = true},
-                  {.column = {"name"},
-                   .data_type = sqldb::DataType::kString,
-                   .unique = true},
-                  {.column = {"base_asset_id"},
-                   .data_type = sqldb::DataType::kInt64,
-                   .foreign_key = {{.table = {"Asset"}, .column = {"id"}}}},
-                  {.column = {"min_base_amount"},
-                   .data_type = sqldb::DataType::kDouble},
-                  {.column = {"base_price_step"},
-                   .data_type = sqldb::DataType::kDouble},
-                  {.column = {"quote_asset_id"},
-                   .data_type = sqldb::DataType::kInt64,
-                   .foreign_key = {{.table = {"Asset"}, .column = {"id"}}}},
-                  {.column = {"min_quote_amount"},
-                   .data_type = sqldb::DataType::kDouble},
-                  {.column = {"quote_price_step"},
-                   .data_type = sqldb::DataType::kDouble}}};
-  return kTable;
+auto SymbolInfo::Definition() -> const sqldb::TableDefinition& {
+  static const auto kDefinition = sqldb::TableDefinition{
+      .table = {kTable},
+      .columns = {
+          {.column = {kId},
+           .data_type = sqldb::DataType::kInt64,
+           .primary_key = true,
+           .auto_increment = true,
+           .unique = true},
+          {.column = {kName},
+           .data_type = sqldb::DataType::kString,
+           .unique = true},
+          {.column = {kBaseAssetId},
+           .data_type = sqldb::DataType::kInt64,
+           .foreign_key = {{.table = {Asset::kTable}, .column = {Asset::kId}}}},
+          {.column = {kBaseAssetMinAmount},
+           .data_type = sqldb::DataType::kDouble},
+          {.column = {kBaseAssetPriceStep},
+           .data_type = sqldb::DataType::kDouble},
+          {.column = {kQuoteAssetId},
+           .data_type = sqldb::DataType::kInt64,
+           .foreign_key = {{.table = {Asset::kTable}, .column = {Asset::kId}}}},
+          {.column = {kQuoteAssetMinAmount},
+           .data_type = sqldb::DataType::kDouble},
+          {.column = {kQuoteAssetPriceStep},
+           .data_type = sqldb::DataType::kDouble}}};
+  return kDefinition;
 }
 
-auto SymbolPriceRecord() -> const sqldb::TableDefinition& {
-  static const auto kTable = sqldb::TableDefinition{
-      .table = {"SymbolPriceRecord"},
-      .columns = {{.column = {"symbol_id"},
+auto SymbolPriceRecord::Definition() -> const sqldb::TableDefinition& {
+  static const auto kDefinition = sqldb::TableDefinition{
+      .table = {kTable},
+      .columns = {{.column = {kSymbolId},
                    .data_type = sqldb::DataType::kInt64,
-                   .foreign_key = {{.table = {"Symbol"}, .column = {"id"}}}},
-                  {.column = {"price"}, .data_type = sqldb::DataType::kDouble},
-                  {.column = {"time"}, .data_type = sqldb::DataType::kInt64}}};
-  return kTable;
+                   .foreign_key = {{.table = {SymbolInfo::kTable},
+                                    .column = {SymbolInfo::kId}}}},
+                  {.column = {kPrice}, .data_type = sqldb::DataType::kDouble},
+                  {.column = {kTime}, .data_type = sqldb::DataType::kInt64}}};
+  return kDefinition;
 }
 }  // namespace stonks::app::sdb::tables
