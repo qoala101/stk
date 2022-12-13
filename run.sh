925ff030df349d9d12c15998f871146632925b9b
Volodymyr Hromakov <4y5t6r@gmail.com>
2022-12-13 16:23:03 +0200

    Added docker files for services. Added thirdparty folder to binaries image.

--- run.sh
+++ run.sh
@@ -1,12 +1,5 @@
-x-terminal-emulator -e ./service_symbols_db \
-  --port 6506 \
-  --db_file_path symbols_db.db &
-
-x-terminal-emulator -e ./service_symbols_db_updater \
-  --update_symbols_info_interval 3600000 \
-  --delete_old_prices_interval 3600000 \
-  --keep_prices_for_duration 3600000 \
-  --reattempt_interval 60000 \
+x-terminal-emulator -e ./service_buy_low_sell_high \
+  --port 6507 \
   --symbols_db_host 0.0.0.0 \
   --symbols_db_port 6506 &
 
@@ -22,7 +15,14 @@ x-terminal-emulator -e ./service_symbol_prices_stream \
   --symbols_db_host 0.0.0.0 \
   --symbols_db_port 6506 &
 
-x-terminal-emulator -e ./service_buy_low_sell_high \
-  --port 6507 \
+x-terminal-emulator -e ./service_symbols_db \
+  --port 6506 \
+  --db_file_path symbols_db.db &
+
+x-terminal-emulator -e ./service_symbols_db_updater \
+  --update_symbols_info_interval 3600000 \
+  --delete_old_prices_interval 3600000 \
+  --keep_prices_for_duration 3600000 \
+  --reattempt_interval 60000 \
   --symbols_db_host 0.0.0.0 \
   --symbols_db_port 6506 &
