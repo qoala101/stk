58a7c902e02609c2436c355ef9b4b93d708fdb08
Volodymyr Hromakov <4y5t6r@gmail.com>
2022-12-14 23:55:16 +0200

    Added initial version of working persistent DB via kubernetes.

--- build.sh
+++ build.sh
@@ -11,17 +11,35 @@ docker build -t qoala101/stonks:dependencies -f ./docker/dependencies/Dockerfile
 # Should be rebuilt when dependencies image or any source files are changed.
 docker build -t qoala101/stonks:binaries -f ./docker/binaries/Dockerfile .
 
-# Build Docker test app images.
+# Build service images.
 # Should be rebuild when binaries image is changed.
-docker compose -f ./src/app/docker_test/docker-compose.yml build
+docker build -t qoala101/stonks:service_buy_low_sell_high -f ./src/app/service/buy_low_sell_high/Dockerfile .
+docker build -t qoala101/stonks:service_symbol_prices_stream -f ./src/app/service/symbol_prices_stream/Dockerfile .
+docker build -t qoala101/stonks:service_symbols_db -f ./src/app/service/symbols_db/Dockerfile .
+docker build -t qoala101/stonks:service_symbols_db_updater -f ./src/app/service/symbols_db_updater/Dockerfile .
 
-# Upload Docker test app images to Docker Hub.
-docker compose -f ./src/app/docker_test/docker-compose.yml push
+# Upload service images to Docker Hub.
+docker push -t qoala101/stonks:service_buy_low_sell_high
+docker push -t qoala101/stonks:service_symbol_prices_stream
+docker push -t qoala101/stonks:service_symbols_db
+docker push -t qoala101/stonks:service_symbols_db_updater
 
-# run kubernetes deployment
-kompose convert
-# use something like all yaml files
-kubectl apply -f pds-tcp-service.yaml,pds-pod.yaml,stec-tcp-service.yaml,stec-pod.yaml
+# Create volume for DBs.
+docker volume create stonks
+
+docker run -v stonks:/app/bin qoala101/stonks:service_symbols_db
+
+# # Build Docker test app images.
+# # Should be rebuild when binaries image is changed.
+# docker compose -f ./src/app/docker_test/docker-compose.yml build
+
+# # Upload Docker test app images to Docker Hub.
+# docker compose -f ./src/app/docker_test/docker-compose.yml push
+
+# # run kubernetes deployment
+# kompose convert
+# # use something like all yaml files
+# kubectl apply -f pds-tcp-service.yaml,pds-pod.yaml,stec-tcp-service.yaml,stec-pod.yaml
 
 # run ngrok which opens port to kubernetes
 
