5425d52d787d4182dbd54b93d70ed64b9db747fe
Volodymyr Hromakov <4y5t6r@gmail.com>
2023-01-31 13:40:31 +0200

    Removed deprecated folder.

--- _deprecated/src/apps/app_ngrok_aws_db_updater.cc
+++
@@ -1,8 +0,0 @@
-#include <cstdio>
-
-#include "stonks_ngrok_aws_db_updater.h"
-
-auto main(int /*unused*/, const char* /*unused*/[]) -> int {
-  const auto app = stonks::NgrokAwsDbUpdater{};
-  static_cast<void>(getchar());
-}
